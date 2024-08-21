516f6a1ce6d2b482e26c1ff195064f891322d312
evan <evan@debian-Evan-Laptop.lan>
2024-08-21 13:43:49 -0400

    added driver manager

--- This_Week_In_Student_Life.py
+++ This_Week_In_Student_Life.py
@@ -1,4 +1,6 @@
 from selenium import webdriver
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.chrome.service import Service as ChromeService
 import Date_Selector
 import Selenium_Request
 import Event
@@ -6,7 +8,7 @@ import Write_To_Docx
 
 dates = Date_Selector.Date_Selector(None, None)
 dates.cal_selector()
-driver =  webdriver.Chrome()
+driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()))
 raw_events = Selenium_Request.Selenium_Request.load_page(driver, dates)
 events_list = Event.Events()
 events_list.pull_elements(raw_events)
